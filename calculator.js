066b3c67a423251efd92a1e95bccdd6e9a8d93e9
Bryan Hankins <bryanhankins@me.com>
2016-08-04 18:10:39 -0500

    added starting html and js content

---
+++ calculator.js
@@ -0,0 +1,24 @@
+var user = document.getElementById("user");
+var output = document.getElementById("output");
+var button = document.getElementById("add")
+button.addEventListener(button, add)
+
+
+function add(a,b) {
+
+return a+b
+console.log(clicked)
+
+}
+
+function subtract() {
+
+}
+
+function multiply() {
+
+}
+
+function divide() {
+
+}
