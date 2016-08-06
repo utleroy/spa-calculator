bc5ae476b632f5099ab09ab98fb366dee4f95628
Bryan Hankins <bryanhankins@me.com>
2016-08-05 23:23:14 -0500

    some functionality working

--- calculator.js
+++ calculator.js
@@ -1,24 +1,35 @@
-var user = document.getElementById("user");
+var input1 = document.getElementById("user1");
+var input2 = document.getElementById("user2");
 var output = document.getElementById("output");
-var button = document.getElementById("add")
-button.addEventListener(button, add)
+var addBtn = document.getElementById("add");
+var subtractBtn = document.getElementById("subtract");
+var multiplyBtn = document.getElementById("multiply");
+var divideBtn = document.getElementById("divide");
+
+addBtn.addEventListener("click", add);
+subtractBtn.addEventListener("click", subtract);
+multiplyBtn.addEventListener("click", multiply);
+divideBtn.addEventListener("click", divide);
+output.addEventListener("click", output);
 
 
-function add(a,b) {
+function userinput() {
 
-return a+b
-console.log(clicked)
+}
 
+function add(c,d) {
+  var added = input1 + input2
+  console.log("clicked add", add)
 }
 
 function subtract() {
-
+  console.log("clicked subtract")
 }
 
 function multiply() {
-
+  console.log("clicked multiply")
 }
 
 function divide() {
-
+  console.log("clicked divide")
 }
